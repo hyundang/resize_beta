3987567aea3bad3e121b3648cc41811a09061f44
hyundang <hyunjin5697@yonsei.ac.kr>
2021-05-27 13:31:20 +0900

    fix: register state

--- src/states/register.js
+++ src/states/register.js
@@ -20,21 +20,3 @@ export const getUserSelector = selector({
     set(UserState, newValue);
   },
 });
-
-import { atom, atomFamily } from "recoil";
-
-export const SignupDataState = atom({
-  key: "SignupDataState",
-  default: {
-    name: "",
-    birth: "",
-    sex: -1,
-    email: "",
-    phone: "",
-    password: "",
-    password_check: "",
-    address: "",
-    address_detail: "",
-    check_list: [],
-  },
-});
